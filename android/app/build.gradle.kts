48a46ebc3fdf407fc4e77b5b6f136ee41177a218
dulaj96 <dulajmithun96@gmail.com>
2026-03-19 09:34:07 +0530

    Update android build config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         applicationId = "com.therockofpraise.lyrics"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = 2
         versionName = "1.0.1"
