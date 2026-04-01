4b01a7e77cc68cb2415d46f2b07bd5930acb54b1
dulaj96 <dulajmithun96@gmail.com>
2026-04-01 21:53:02 +0530

    Fix persistent sqflite MissingPluginException: add explicit registration to MainActivity and upgrade sqflite version

--- android/app/src/main/kotlin/com/therockofpraise/lyrics/MainActivity.kt
+++ android/app/src/main/kotlin/com/therockofpraise/lyrics/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.therockofpraise.lyrics
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterActivity(){
-    
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // Explicitly register plugins to ensure sqflite and others are linked
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
