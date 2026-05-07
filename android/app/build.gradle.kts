0741d9c19a8b70d254e40ecefd84e1ba35681724
dulaj96 <dulajmithun96@gmail.com>
2026-05-07 12:30:29 +0530

    fix: enable core library desugaring for Android compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,6 +46,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -105,6 +106,7 @@ android {
 
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
 }
 
 
